1b7e860d5f3c7b8f89eb384ffaa94bfbda5468bb
suyash kumar singh <suyashkumar.singh@tesco.com>
2021-03-06 00:41:47 +0530

    Adds logger

--- src/main/java/com/poc/Application.java
+++ src/main/java/com/poc/Application.java
@@ -2,26 +2,22 @@ package com.poc;
 
 import com.poc.inputorder.InputOrderAvroProducer;
 import com.poc.inputorder.InputOrderProducer;
-import com.poc.inputorder.avro.InputOrderAvro;
-import com.poc.topology.InputToOutputTopology;
 import com.poc.topology.InputToPeekTopology;
-
-import static java.lang.System.out;
 
 public class Application {
 
-    public static void main(String[] args) {
-//        int recordCount = Integer.parseInt(args[0]);
-
-//        InputOrderProducer inputOrderProducer = new InputOrderProducer();
-//        inputOrderProducer.start(recordCount);
+    public static void main(String[] args) throws InterruptedException {
+        int recordCount = Integer.parseInt(args[0]);
 //
-//        InputOrderAvroProducer inputOrderAvroProducer = new InputOrderAvroProducer();
-//        inputOrderAvroProducer.start(recordCount);
+        InputOrderProducer inputOrderProducer = new InputOrderProducer();
+        inputOrderProducer.start(recordCount);
+
+        InputOrderAvroProducer inputOrderAvroProducer = new InputOrderAvroProducer();
+        inputOrderAvroProducer.start(recordCount);
 
 //        InputToOutputTopology topology = new InputToOutputTopology();
         InputToPeekTopology topology = new InputToPeekTopology();
-        topology.start();
+//        topology.start();
 
         Runtime
                 .getRuntime()
