5d6ffd775b104fcb0d8fae704855e70cadb5c84a
suyash kumar singh <suyashkumar.singh@tesco.com>
2021-03-05 19:27:52 +0530

    Fixes input order producer key serializer

--- src/main/java/com/poc/inputorder/InputOrderProducer.java
+++ src/main/java/com/poc/inputorder/InputOrderProducer.java
@@ -1,7 +1,6 @@
 package com.poc.inputorder;
 
 import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +12,7 @@ public class InputOrderProducer {
     public Properties getConfig() {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, InputOrderKeySerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, InputOrderSerializer.class.getName());
         return props;
     }
