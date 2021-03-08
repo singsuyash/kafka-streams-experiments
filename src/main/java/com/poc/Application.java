fc2286d1837e0ef04cd202beb70d7989fee12c3f
suyash kumar singh <suyashkumar.singh@tesco.com>
2021-03-08 18:44:11 +0530

    Adds spring boot, spring actuator in the mix

--- src/main/java/com/poc/Application.java
+++ src/main/java/com/poc/Application.java
@@ -1,10 +1,14 @@
 package com.poc;
 
-import com.poc.topology.InputOrderAndDiagnosticJoinToPeekTopology;
+import com.poc.topology.InputToOutputTopology;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(Application.class, args);
 //        int recordCount = Integer.parseInt(args[0]);
 
 //        InputOrderProducer inputOrderProducer = new InputOrderProducer();
@@ -16,11 +20,11 @@ public class Application {
 //        InputOrderAndDiagnosticAvroProducer inputOrderAndDiagnosticAvroProducer = new InputOrderAndDiagnosticAvroProducer();
 //        inputOrderAndDiagnosticAvroProducer.start(recordCount);
 
-//        InputToOutputTopology topology = new InputToOutputTopology();
+        InputToOutputTopology topology = new InputToOutputTopology();
 //        InputToPeekTopology topology = new InputToPeekTopology();
 //        InputToPeekAvroTopology topology = new InputToPeekAvroTopology();
 //        InputToOutputAvroTopology topology = new InputToOutputAvroTopology();
-        InputOrderAndDiagnosticJoinToPeekTopology topology = new InputOrderAndDiagnosticJoinToPeekTopology();
+//        InputOrderAndDiagnosticJoinToPeekTopology topology = new InputOrderAndDiagnosticJoinToPeekTopology();
 
         topology.start();
 
